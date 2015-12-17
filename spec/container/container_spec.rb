5fb68c9ee3d1db4e82301c0c7493d9d058865f91
olhtbr <olhtbr@gmail.com>
2015-12-17 09:27:27 +0000

    Add more tests for docker image

--- spec/container/container_spec.rb
+++ spec/container/container_spec.rb
@@ -2,4 +2,6 @@ require 'spec_helper'
 
 describe docker_image('olhtbr/p4-resource:latest') do
   it { should exist }
+  its(['Architecture']) { should eq 'amd64' }
+  its(['Os']) { should eq 'linux' }
 end
