858900a7f09d707220e9cc65e643cedac1310d49
olhtbr <olhtbr@gmail.com>
2016-01-02 21:01:58 +0100

    Add failing test

--- check/check_test.go
+++ check/check_test.go
@@ -1,9 +1,18 @@
 package main_test
 
 import (
+	"github.com/olhtbr/p4-resource/models"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Check", func() {
+var _ = Describe("Check executed", func() {
+	var response models.CheckResponse
 
+	Context("when version is omitted", func() {
+		It("should return the latest version", func() {
+			Expect(response).To(HaveLen(1))
+			Expect(response[0].Changelist).To(Equal("123456"))
+		})
+	})
 })
