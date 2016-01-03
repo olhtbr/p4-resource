9671eaafb2768b9b4c424f6477aabc6364821808
olhtbr <olhtbr@gmail.com>
2016-01-03 23:05:47 +0100

    Make empty input version test pass

--- check/check_test.go
+++ check/check_test.go
@@ -7,21 +7,16 @@ import (
 	"github.com/olhtbr/p4-resource/models"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
 )
 
 var _ = Describe("Check executed", func() {
-	var jsonBlob []byte
 	var cmd *exec.Cmd
 	var request models.CheckRequest
 	var response models.CheckResponse
 
-	JustBeforeEach(func() {
-		err := json.Unmarshal(jsonBlob, &request)
-		Expect(err).To(Not(HaveOccurred()))
-	})
-
 	BeforeEach(func() {
-		jsonBlob = []byte(`{
+		jsonBlob := []byte(`{
 			"source": {
 				"port": {
 					"protocol": "ssl",
@@ -39,7 +34,26 @@ var _ = Describe("Check executed", func() {
 			"version": {"changelist": "123456"}
 		}`)
 
-		cmd = exec.Command("bin/check")
+		err := json.Unmarshal(jsonBlob, &request)
+		Expect(err).To(Not(HaveOccurred()))
+
+		cmd = exec.Command("../bin/check")
+	})
+
+	JustBeforeEach(func() {
+		stdin, err := cmd.StdinPipe()
+		Expect(err).To(Not(HaveOccurred()))
+
+		session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+		Expect(err).To(Not(HaveOccurred()))
+
+		err = json.NewEncoder(stdin).Encode(request)
+		Expect(err).To(Not(HaveOccurred()))
+
+		Eventually(session).Should(gexec.Exit(0))
+
+		err = json.Unmarshal(session.Out.Contents(), &response)
+		Expect(err).To(Not(HaveOccurred()))
 	})
 
 	Context("when version is omitted", func() {
@@ -49,7 +63,6 @@ var _ = Describe("Check executed", func() {
 		})
 
 		It("should return the latest version", func() {
-
 			Expect(response).To(HaveLen(1))
 			Expect(response[0].Changelist).To(Equal("123456"))
 		})
--- check/main.go
+++ check/main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-  "fmt"
+	"encoding/json"
+	"log"
+	"os"
+
+	"github.com/olhtbr/p4-resource/models"
 )
 
 func main() {
-  fmt.Println("hello, check")
+	var request models.CheckRequest
+	var response models.CheckResponse
+
+	err := json.NewDecoder(os.Stdin).Decode(&request)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if request.Version.Changelist == "" {
+		// TODO Get latest changelist from the driver
+		response = models.CheckResponse{
+			{Changelist: "123456"},
+		}
+	}
+
+	json.NewEncoder(os.Stdout).Encode(response)
 }
