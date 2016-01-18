0522a6c2304d0771721db98c941156f0b125c930
olhtbr <olhtbr@gmail.com>
2016-01-18 22:12:49 +0100

    Checking for empty list if the input version is the latest changelist

--- check/check_test.go
+++ check/check_test.go
@@ -16,6 +16,24 @@ var _ = Describe("Check executed", func() {
 	var cmd *exec.Cmd
 	var request models.CheckRequest
 	var response models.CheckResponse
+	var cl string
+
+	GetChangelistCounter := func() uint64 {
+		cmd := exec.Command("p4", "-z", "tag", "-u", "Joe_Coder", "-p", "localhost:1666", "counter", "change")
+		out, err := cmd.Output()
+		Expect(err).To(Not(HaveOccurred()))
+		Expect(out).To(Not(BeNil()))
+		Expect(out).To(Not(BeEmpty()))
+
+		re := regexp.MustCompile(`\.\.\.\s+value\s+(\d+)`)
+		match := re.FindStringSubmatch(string(out))
+		Expect(len(match)).To(Equal(2))
+
+		counter, err := strconv.ParseUint(match[1], 10, 16)
+		Expect(err).To(Not(HaveOccurred()))
+
+		return counter
+	}
 
 	BeforeEach(func() {
 		jsonBlob := []byte(`{
@@ -59,37 +77,37 @@ var _ = Describe("Check executed", func() {
 	})
 
 	Context("when version is omitted", func() {
-		var cl string
-		var cmd *exec.Cmd
-
 		BeforeEach(func() {
 			request.Version.Changelist = ""
 			request.Source.Filespec.Depot = "..."
 			request.Source.Filespec.Stream = "..."
 			request.Source.Filespec.Path = "..."
-		})
-
-		JustBeforeEach(func() {
-			cmd = exec.Command("p4", "-z", "tag", "-u", "Joe_Coder", "-p", "localhost:1666", "counter", "change")
-			out, err := cmd.Output()
-			Expect(err).To(Not(HaveOccurred()))
-			Expect(out).To(Not(BeNil()))
-			Expect(out).To(Not(BeEmpty()))
-
-			re := regexp.MustCompile(`\.\.\.\s+value\s+(\d+)`)
-			match := re.FindStringSubmatch(string(out))
-			Expect(len(match)).To(Equal(2))
 
 			// Counter is latest changelist + 1
-			counter, err := strconv.ParseUint(match[1], 10, 16)
-			Expect(err).To(Not(HaveOccurred()))
-
+			counter := GetChangelistCounter()
 			cl = strconv.FormatUint(uint64(counter-1), 10)
 		})
 
 		It("should return the latest version", func() {
 			Expect(response).To(HaveLen(1))
 			Expect(response[0].Changelist).To(Equal(cl))
+		})
+	})
+
+	Context("when version is latest", func() {
+		BeforeEach(func() {
+			// Counter is latest changelist + 1
+			counter := GetChangelistCounter()
+			cl = strconv.FormatUint(uint64(counter-1), 10)
+
+			request.Version.Changelist = cl
+			request.Source.Filespec.Depot = "..."
+			request.Source.Filespec.Stream = "..."
+			request.Source.Filespec.Path = "..."
+		})
+
+		It("should return an empty list", func() {
+			Expect(response).To(HaveLen(0))
 		})
 	})
 })
